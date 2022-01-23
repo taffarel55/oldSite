f10c01ac55d52538ed0a1faad61f15a39d129dd5
Maurício <mauruciotaffarel@gmail.com>
2022-01-23 19:07:27 -0300

    Funcionalidade do markdown

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,8 +1,31 @@
 import "./index.css";
+import "../../settings/markdown/index.css";
 import Page from "../../components/Page";
+import Markdown from "markdown-to-jsx";
+import { useState, useEffect } from "react";
 
+// TODO: Ver se é possível criar um hook useMarkdown para retornar somente o post
 const About = () => {
-  return <Page></Page>;
+  const [post, setPost] = useState("");
+  const fileName = "index.pt-br.md";
+  useEffect(() => {
+    import(`./content/${fileName}`)
+      .then((res) => {
+        fetch(res.default)
+          .then((res) => res.text())
+          .then((res) => setPost(res))
+          .catch((err) => console.log(err));
+      })
+      .catch((err) => console.log(err));
+  });
+
+  return (
+    <Page>
+      <div className="Page__markdown">
+        <Markdown>{post}</Markdown>
+      </div>
+    </Page>
+  );
 };
 
 export default About;
